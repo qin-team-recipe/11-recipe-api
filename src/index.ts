2fc7c45a6fea8acff6446c7d5550216a2d2fe3b3
aguriken <ohagurokenichi@gmail.com>
2023-06-20 16:53:20 +0900

    🧑‍💻 API起動のPORT変更

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const port = 8080;
+const port = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
