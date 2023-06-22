8f71cb59cfb0b4588a5bf5855c8aba8b864353cd
ryutaroda <56154082+ryutaroda@users.noreply.github.com>
2023-06-23 00:00:26 +0900

    18 chef crud api.

--- src/controllers/chef.controller.ts
+++ src/controllers/chef.controller.ts
@@ -1,20 +1,189 @@
 import { NextFunction, Request, Response, Router } from "express";
-import { prisma } from "../../prisma/prisma-client";
+import { prisma, Chef } from "../../prisma/prisma-client";
 
 const router = Router();
 
 /**
  * Get chefs
  * @route {GET} /chefs
+ * @param {string} role
  * @returns chefs
  */
 router.get(
   "/chefs",
   async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const response = await prisma.chef.findMany();
-      console.log("aaa");
-      res.json(response);
+      const { role } = req.query;
+
+      let chefs: Chef[] = await prisma.chef.findMany({
+        include: {
+          _count: {
+            // _count は、リレーション先のレコード数を取得する
+            select: { recipes: true },
+          },
+        },
+        orderBy: {
+          name: "asc", // 名前を昇順で並び替える
+        },
+      });
+
+      if (role) {
+        chefs = chefs.filter((chef: Chef) => chef.role === role);
+      }
+
+      if (chefs.length === 0) {
+        res.status(404).json({ message: "Chefs not found" });
+      } else {
+        res.json(chefs);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+/**
+ * Post chef
+ * @route {POST} /chefs
+ * @returns chef
+ */
+router.post(
+  "/chefs",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const {
+        name,
+        role,
+        userId,
+        profile,
+        imageUrl,
+      }: {
+        name: string;
+        role: "USER" | "CHEF";
+        userId: string | null;
+        profile: string | null;
+        imageUrl: string | null;
+      } = req.body;
+
+      if (!name || !role) {
+        res.status(400).json({ message: "Name, and role are required" });
+        return;
+      }
+
+      const chef = await prisma.chef.create({
+        data: {
+          name: name,
+          role: role,
+          userId: userId,
+          profile: profile,
+          imageUrl: imageUrl,
+        },
+      });
+      res.json(chef);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+/**
+ * Get chef
+ * @route {GET} /chefs/:id
+ * @returns chef
+ */
+router.get(
+  "/chefs/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const chef = await prisma.chef.findUnique({
+        where: {
+          id: String(req.params.id),
+        },
+        include: {
+          recipes: true, // リレーション先のレコードを取得する
+          _count: {
+            select: {
+              recipes: true,
+              follows: true,
+            },
+          },
+        },
+      });
+      if (!chef) {
+        res.status(404).json({ message: "Chef not found" });
+      } else {
+        res.json(chef);
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+/**
+ * Put chef
+ * @route {PUT} /chefs/:id
+ * @returns chef
+ */
+router.put(
+  "/chefs/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const {
+        name,
+        role,
+        userId,
+        profile,
+        imageUrl,
+      }: {
+        name: string;
+        role: "USER" | "CHEF";
+        userId: string | null;
+        profile: string | null;
+        imageUrl: string | null;
+      } = req.body;
+
+      if (!name || !role) {
+        res.status(400).json({ message: "Name, and role are required" });
+        return;
+      }
+
+      const chef = await prisma.chef.update({
+        where: {
+          id: String(req.params.id),
+        },
+        data: {
+          name: name,
+          role: role,
+          userId: userId,
+          profile: profile,
+          imageUrl: imageUrl,
+        },
+      });
+
+      res.json(chef);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+/**
+ * Delete chef
+ * @route {DELETE} /chefs/:id
+ * @returns chef
+ */
+router.delete(
+  "/chefs/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const chef = await prisma.chef.delete({
+        where: {
+          id: String(req.params.id),
+        },
+      });
+
+      res.json({ message: "Deleted chef" });
     } catch (error) {
       next(error);
     }
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,18 @@ import express, { Request, Response } from "express";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import routes from "./routes/routes";
+import bodyParser from "body-parser";
 
 const app = express();
 
 const port = 8080;
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 app.use(routes);
 
 // Swagger options
