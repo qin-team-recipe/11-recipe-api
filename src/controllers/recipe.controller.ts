d1b85f37fa2ee344b9514543d97e297e32742c96
Takuma Yoshimi <ysm0706glee@gmail.com>
2023-07-01 16:13:32 +0200

    add recipe api

--- src/controllers/recipe.controller.ts
+++ src/controllers/recipe.controller.ts
@@ -1,21 +1,85 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Handler, Request, Response, Router } from "express";
+
+import { prisma, Chef, Recipe } from "../../prisma/prisma-client";
 
 const router = Router();
 
+type CreateRecipeBody = {
+  chefId: Chef["id"];
+  name?: Recipe["name"];
+  overview?: Recipe["overview"];
+  servingSize: Recipe["servingSize"];
+  status: Recipe["status"];
+};
+
 /**
- * Post recipe
- * @route {POST} /recipe
- * @returns
+ * Get recipes
+ * @route {GET} /recipes
+ * @returns recipes
  */
-router.post(
-  "/recipes",
-  async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      res.json({ message: "Created recipe" });
-    } catch (error) {
-      next(error);
-    }
+export const getRecipes: Handler = async (req: Request, res: Response) => {
+  try {
+    const recipes = await prisma.recipe.findMany();
+    return res.json(recipes);
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json({ message: "Recipes not found" });
   }
-);
+};
+
+/**
+ * Get recipe
+ * @route {GET} /recipes/:id
+ * @returns recipe
+ */
+export const getRecipe: Handler = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ message: "Recipe id not found" });
+    }
+    const recipe = await prisma.recipe.findUnique({
+      where: { id },
+    });
+    if (recipe) {
+      return res.json(recipe);
+    }
+    return res.status(404).json({ message: "Recipe not found" });
+  } catch (error) {
+    console.error(error);
+    return res.status(404).json({ message: "Recipe not found" });
+  }
+};
+
+/**
+ * Create recipe
+ * @route {POST} /recipes
+ * @returns { message: "Recipe created" }
+ */
+export const createRecipe: Handler = async (req: Request, res: Response) => {
+  try {
+    const { chefId, name, overview, servingSize, status }: CreateRecipeBody =
+      req.body;
+    const chef = await prisma.chef.findUnique({
+      where: { id: chefId },
+    });
+    if (!chef) {
+      return res.status(404).json({ message: "Chef not found" });
+    }
+    await prisma.recipe.create({
+      data: {
+        chefId,
+        name,
+        overview,
+        servingSize,
+        status,
+      },
+    });
+    return res.json({ message: "Recipe created" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Recipe not created" });
+  }
+};
 
 export default router;
---
+++ src/routes/recipe.routes.ts
@@ -0,0 +1,98 @@
+import { Router } from "express";
+import {
+  getRecipes,
+  getRecipe,
+  createRecipe,
+} from "../controllers/recipe.controller";
+
+export const router = Router();
+
+/**
+ * @swagger
+ * /recipes:
+ *   get:
+ *     summary: Return a recipe list
+ *     tags:
+ *       - recipe
+ *     description: Get a list of recipes
+ *     operationId: getRecipes
+ *     responses:
+ *       200:
+ *         description: The list of recipes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/recipe'
+ *       404:
+ *         description: Recipes not found
+ */
+
+router.get("/recipes", getRecipes);
+
+/**
+ * @swagger
+ * /recipes/{id}:
+ *   get:
+ *     summary: Return a recipe
+ *     tags:
+ *       - recipe
+ *     description: Get a recipe
+ *     operationId: getRecipe
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: recipe id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       description: Recipe object that needs to be added to the store
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/recipe'
+ *     responses:
+ *       200:
+ *         description: The recipe
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/recipe'
+ *       404:
+ *         description: Recipe id not found or Recipe not found
+ */
+
+router.get("/recipes/:id", getRecipe);
+
+/**
+ * @swagger
+ * /recipes:
+ *   post:
+ *     summary: Create a new recipe
+ *     tags:
+ *       - recipe
+ *     description: Create a new recipe
+ *     operationId: createRecipe
+ *     requestBody:
+ *       description: Recipe object that needs to be added to the store
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/recipe'
+ *     responses:
+ *       200:
+ *         description: Created recipe
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/recipe'
+ *       400:
+ *         description: Chef not found
+ *       500:
+ *         description: Recipe not created
+ */
+router.post("/recipes", createRecipe);
