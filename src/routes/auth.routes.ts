b115e9e9d86fd1cf69e8970038720704f711b1bb
Takuma Yoshimi <ysm0706glee@gmail.com>
2023-07-22 12:09:15 +0200

    add google auth

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,62 @@
+import { Request, Response, NextFunction, Router } from "express";
+import passport from "passport";
+import { prisma } from "../../prisma/prisma-client";
+
+export const router = Router();
+
+const isLoggedIn = (req: Request, res: Response, next: NextFunction) => {
+  // TODO: passportで良い？
+  req.session.passport ? next() : res.sendStatus(401);
+};
+
+passport.serializeUser((user: any, done: any) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user: any, done: any) => {
+  done(null, user);
+});
+
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/api/v1/auth/google/success",
+    failureRedirect: "/api/v1/auth/google/failure",
+  })
+);
+
+router.get(
+  "/auth/google/success",
+  isLoggedIn,
+  async (req: Request, res: Response) => {
+    try {
+      const googleId = req.user?.id;
+      const user = await prisma.user.findUnique({
+        where: { googleId },
+      });
+      if (!user) {
+        return res.status(404).send("User not found");
+      }
+      // ニックネームがない場合はregisterに飛ばす
+      if (!user.name) {
+        return res.redirect("/register");
+      }
+      res.redirect("/");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+);
+
+router.get("/auth/google/failure", async (req: Request, res: Response) => {
+  res.send("failure");
+});
+
+router.use("/auth/logout", (req, res) => {
+  req.session.destroy(() => res.redirect("/"));
+});
